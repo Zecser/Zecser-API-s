39189d3115d468039dd5f0903c44c1fcf26088d7
aravind <aravind284479@gmail.com>
2025-10-30 13:40:05 +0530

    feat : razorpay

--- razorpay/frontend/src/api/paymentApi.js
+++ razorpay/frontend/src/api/paymentApi.js
@@ -1,10 +1,13 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_BASE_URL = 'http://localhost:5000/api/payment';
+const API_BASE_URL =
+  "https://razorpay-sxsq.onrender.com" || "http://localhost:5000/api/payment";
 
 export const createOrder = async (amount) => {
   try {
-    const response = await axios.post(`${API_BASE_URL}/create-order`, { amount });
+    const response = await axios.post(`${API_BASE_URL}/create-order`, {
+      amount,
+    });
     return response.data;
   } catch (error) {
     throw error.response?.data || error.message;
